00a429cff3e6caa8df6663f8807d231092e75dc1
MarcT03 <134474146+MarcT03@users.noreply.github.com>
2024-06-28 23:24:57 -0700

    Fixed a few bugs to make it run on Discord

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Imports the req modules
+from typing import Final
 import discord
 import os
 from discord.ext import commands
@@ -6,18 +7,27 @@ from dotenv import load_dotenv
 
 # This makes a Discord client instance and sets the command prefix
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} is logged in and ready to take commands.")
+    print(f"{bot.user} is logged in and ready to take commands.")
 
-bot.command()
-async def greet(cmd):
-    response: "Hello, I'm {bot.user.name}, this servers discord bot."
-    await cmd.send(response)
+
+@bot.command()
+async def greet(ctx):
+    response = f"Hello, I'm {bot.user.name}, this servers discord bot."
+    await ctx.send(response)
 
 
 load_dotenv()
-bot.run(os.getenv('TOKEN'))
+DISCORD_TOKEN: Final[str] = os.getenv('TOKEN')
+
+
+def main() -> None:
+    bot.run(token=DISCORD_TOKEN)
+
+
+if __name__ == '__main__':
+    main()
