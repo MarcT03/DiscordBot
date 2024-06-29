8b4cbd64ed2e0999d22440f56c4ffef7af2e6cad
MarcT03 <134474146+MarcT03@users.noreply.github.com>
2024-06-28 18:46:15 -0700

    Added a greet and test command

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ from dotenv import load_dotenv
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is logged in and ready to take commands.")
+
+bot.command()
+async def greet(cmd):
+    response: "Hello, I'm {bot.user.name}, this servers discord bot."
+    await cmd.send(response)
+
+
+load_dotenv()
+bot.run(os.getenv('TOKEN'))
